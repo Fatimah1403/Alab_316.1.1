4fc6d9ee09bef65bed40f80d08bd369db8e60b50
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-10-25 09:28:23 -0400

    working with DOM

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // import "./styles.css";
 // Menu data structure
+const log = console.log;
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 
 let mainEl = document.querySelector("main");
@@ -31,3 +43,65 @@ menuLinks.forEach(link => {
    anchor.textContent = link.text;
    topMenuEl.appendChild(anchor);
 })
+//---------------------------------------------------------------------------------//
+//--- part2--Creating the Submenu--------of the LAB
+let subMenuEl = document.getElementById("sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.setProperty("background-color", "var(--sub-menu-bg)")
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+//select the a tag in topMenuEl.---
+ let topMenuLinks = topMenuEl.querySelectorAll("a");
+topMenuEl.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (event.target.tagName !== "A") 
+    return;
+
+  topMenuLinks.forEach(function(link) {
+    link.classList.remove("active");
+  });
+  event.target.classList.toggle("active");
+  log(event.target.textContent);
+// getting the link in menuLinks.
+//const linkObj = menuLinks.find(link => link.link === event.target.textContent);
+
+  const linkObj = {};
+  for (let i = 0; i < menuLinks.length; i++) {
+    if (menuLinks[i].text === event.target.textContent) {
+      linkObj = menuLinks[i];
+      break;
+    }
+  }
+  if (linkObj && !event.target.classList.contains("active")) {
+    if (linkObj.subLinks) {
+      subMenuEl.style.top = "100%";
+    } else {
+      subMenuEl.style.top = "0";
+    }
+  }
+  function clearSubMenuEl() {
+    subMenuEl.innerHTML = "";
+  }
+  
+  function buildSubMenu(subLinks) {
+    clearSubMenuEl();
+
+   const anchor = document.createElement("a");
+    subLinks.forEach(link => {
+      anchor.setAttribute("href", link.href);
+      anchor.text = link.textContent;
+      subMenuEl.appendChild(anchor);
+
+    })
+  }
+  buildSubMenu();
+
+  subMenuEl.style.top = "0";
+  topMenuLinks.forEach(function (anchor) {
+    anchor.classList.remove("active");
+  });
+  mainEl.innerHTML = <h1>${event.target.textContent}</h1>
+});
+
